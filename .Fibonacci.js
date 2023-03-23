77e7e841880ee207e8bff975c8a0b8cc74a37f23
RodrigoF90 <rodrigoguedes@edu.unifor.br>
2023-03-23 15:53:03 -0300

    git add git commit -m "Fibonacci" git push origin master git add git commiti -m "Trabalhos Unifor" git push origin master git push Trabalhos Unifor

--- .Fibonacci.js
+++ .Fibonacci.js
@@ -1,18 +1,31 @@
-var n = parseInt(prompt());
-var fib = [];
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Fibonacci</title>
+  </head>
+  <body>
+    <p id="fibSeries"></p>
 
-fib[0] = 0;
-fib[1] = 1;
+    <script>
+      var n = parseInt(prompt("Enter the number of terms: "));
+      var fib = [];
 
-for (i=2; i<n; i++) {
-  fib[i] = fib[i-2] + fib[i-1];
-}
+      fib[0] = 0;
+      fib[1] = 1;
 
-var serieFib = '';
+      for (i=2; i<n; i++) {
+        fib[i] = fib[i-2] + fib[i-1];
+      }
 
-for (i=0; i<n-1; i++) {
-  serieFib += fib[i] + ' ';
-}
+      var serieFib = '';
 
-serieFib += fib[n-1]
-console.log(serieFib);
+      for (i=0; i<n-1; i++) {
+        serieFib += fib[i] + ' ';
+      }
+
+      serieFib += fib[n-1];
+      
+      document.getElementById("fibSeries").innerHTML = serieFib;
+    </script>
+  </body>
+</html>
