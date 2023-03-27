6eb200437467d4b2fcb943818ff59feeb23b8a10
RodrigoF90 <rodrigoguedes@edu.unifor.br>
2023-03-27 16:44:02 -0300

    git add

--- .Fibonacci.js
+++ .Fibonacci.js
@@ -1,3 +1,9 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 function fibonacci(n) {
   if (n <= 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
@@ -6,3 +12,7 @@ function fibonacci(n) {
 for (let i = 0; i < 20; i++) {
   console.log(fibonacci(i));
 }
+rl.question('Digite um número: ', (n) => {
+  console.log(fibonacci(n));
+  rl.close();
+});
--- Contagem.js
+++ Contagem.js
@@ -1,13 +1,24 @@
+const { stdout } = require('process');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 function countIntegers(data, N) {
   let count = 0;
   for (let i = 0; i < data.length; i++) {
       if (data[i] >= data[0] && data[i] <= N) {
           count++;
-      }
+     }
   }
   return count;
 }
-let data = [1, 2, 3, 5, 7, 9, 12, 15, 26];
-let N = 8;
-let result = countIntegers(data, N);
-console.log(result);
+
+rl.question('insira o valor de N: ', (N) => {
+  let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let result = countIntegers(data, N);
+  console.log(result);
+  rl.close();
+
+});
